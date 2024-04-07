6cf6ef0bc19a24214b29fa72cb7b118a942f72df
Shubham Rathore <sr1849587@gmail.com>
2024-04-07 18:53:22 +0530

    Add some more animation

---
+++ index.js
@@ -0,0 +1,103 @@
+document.querySelector(".left")
+.addEventListener("mousemove", function(){
+    document.querySelector(".c-center-one").style.height = "120vh"
+    document.querySelector(".left").style.cursor = "pointer"
+    document.querySelector("#btn").style.backgroundColor = "#333"
+    document.querySelector("#btn").style.color = "#fff"
+    document.querySelector(".image1").style.width = "25vw"
+    document.querySelector(".image1").style.height = "25vh"
+    document.querySelector(".image1").style.opacity = "1"
+    document.querySelector(".image1").style.filter = "blur(0.5px)"
+    document.querySelector("#img2").style.width = "20vw"
+    document.querySelector("#img2").style.height = "20vh"
+    document.querySelector("#img2").style.opacity = "1"
+    document.querySelector("#img2").style.filter = "blur(1px)"
+    document.querySelector("#img3").style.width = "17vw"
+    document.querySelector("#img3").style.height = "17vh"
+    document.querySelector("#img3").style.opacity = "1"
+    document.querySelector("#img3").style.filter = "blur(1.5px)"
+    document.querySelector("#img4").style.width = "13vw"
+    document.querySelector("#img4").style.height = "13vh"
+    document.querySelector("#img4").style.opacity = "1"
+    document.querySelector("#img4").style.filter = "blur(2px)"
+
+
+})
+
+document.querySelector(".left")
+.addEventListener("mouseleave", function(){
+    document.querySelector(".c-center-one").style.height = "0vh"
+    document.querySelector("#btn").style.backgroundColor = "initial"
+    document.querySelector("#btn").style.color = "#333"
+    document.querySelector(".image1").style.width = "20vw"
+    document.querySelector(".image1").style.height = "20vh"
+    document.querySelector(".image1").style.opacity = "0"
+    document.querySelector(".image1").style.filter = "blur(10px)"
+    document.querySelector("#img2").style.width = "15vw"
+    document.querySelector("#img2").style.height = "15vh"
+    document.querySelector("#img2").style.opacity = "0"
+    document.querySelector("#img2").style.filter = "blur(20px)"
+    document.querySelector("#img3").style.width = "10vw"
+    document.querySelector("#img3").style.height = "10vh"
+    document.querySelector("#img3").style.opacity = "0"
+    document.querySelector("#img3").style.filter = "blur(30px)"
+    document.querySelector("#img4").style.width = "5vw"
+    document.querySelector("#img4").style.height = "5vh"
+    document.querySelector("#img4").style.opacity = "0"
+    document.querySelector("#img4").style.filter = "blur(40px)"
+
+
+
+    
+})
+
+document.querySelector(".right")
+.addEventListener("mousemove", function(){
+    document.querySelector(".c-center-two").style.height = "120vh"
+    document.querySelector(".right").style.cursor = "pointer"
+    document.querySelector("#btn2").style.backgroundColor = "#333"
+    document.querySelector("#btn2").style.color = "#fff"
+    document.querySelector("#img5").style.width = "25vw"
+    document.querySelector("#img5").style.height = "25vh"
+    document.querySelector("#img5").style.opacity = "1"
+    document.querySelector("#img5").style.filter = "blur(0.5px)"
+    document.querySelector("#img6").style.width = "20vw"
+    document.querySelector("#img6").style.height = "20vh"
+    document.querySelector("#img6").style.opacity = "1"
+    document.querySelector("#img6").style.filter = "blur(1px)"
+    document.querySelector("#img7").style.width = "17vw"
+    document.querySelector("#img7").style.height = "17vh"
+    document.querySelector("#img7").style.opacity = "1"
+    document.querySelector("#img7").style.filter = "blur(1.5px)"
+    document.querySelector("#img8").style.width = "13vw"
+    document.querySelector("#img8").style.height = "13vh"
+    document.querySelector("#img8").style.opacity = "1"
+    document.querySelector("#img8").style.filter = "blur(2px)"
+    
+
+})
+
+document.querySelector(".right")
+.addEventListener("mouseleave", function(){
+    document.querySelector(".c-center-two").style.height = "0vh"
+    document.querySelector("#btn2").style.backgroundColor = "initial"
+    document.querySelector("#btn2").style.color = "#333"
+    document.querySelector("#img5").style.width = "20vw"
+    document.querySelector("#img5").style.height = "20vh"
+    document.querySelector("#img5").style.opacity = "0"
+    document.querySelector("#img5").style.filter = "blur(10px)"
+    document.querySelector("#img6").style.width = "15vw"
+    document.querySelector("#img6").style.height = "15vh"
+    document.querySelector("#img6").style.opacity = "0"
+    document.querySelector("#img6").style.filter = "blur(20px)"
+    document.querySelector("#img7").style.width = "10vw"
+    document.querySelector("#img7").style.height = "10vh"
+    document.querySelector("#img7").style.opacity = "0"
+    document.querySelector("#img7").style.filter = "blur(30px)"
+    document.querySelector("#img8").style.width = "5vw"
+    document.querySelector("#img8").style.height = "5vh"
+    document.querySelector("#img8").style.opacity = "0"
+    document.querySelector("#img8").style.filter = "blur(40px)"
+
+    
+})
